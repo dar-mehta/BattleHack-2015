d848f6dd992eb923fd9fb30fb54875a908269533
Dar Mehta <dmehta97@gmail.com>
2015-07-19 08:51:32 -0400

    deleted extraneous NFC code

--- app/src/main/java/com/wesleyyue/battlehack2015/NFCActivity.java
+++ app/src/main/java/com/wesleyyue/battlehack2015/NFCActivity.java
@@ -32,11 +32,7 @@ import java.util.Arrays;
 
 public class NFCActivity extends Activity {
 
-    private static final String TAG = "anything";
     NfcAdapter nfcDevice;
-    private PendingIntent mPendingIntent;
-    private IntentFilter[] mIntentFilters;
-    private String[][] mNFCTechLists;
 
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -45,7 +41,6 @@ public class NFCActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
 
-        System.out.println("hello");
         nfcDevice = NfcAdapter.getDefaultAdapter(this);
         nfcDevice.setNdefPushMessageCallback(new CreateNdefMessageCallback() {
             @Override
@@ -56,71 +51,22 @@ public class NFCActivity extends Activity {
 
         }, this, this);
 
-        mPendingIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-        IntentFilter ndefIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        try {
-            ndefIntent.addDataType("*/*");
-            mIntentFilters = new IntentFilter[] { ndefIntent };
-        } catch (Exception e) {
-            Log.e("TagDispatch", e.toString());
-        }
-
-        mNFCTechLists = new String[][] { new String[] { NFCActivity.class.getName() } };
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (nfcDevice != null)
-            nfcDevice.enableForegroundDispatch(this, mPendingIntent, mIntentFilters, mNFCTechLists);
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
 
-        String action = intent.getAction();
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-
-        String s = action + "\n\n" + tag.toString();
-
-        // parse through all NDEF messages and their records and pick text type only
-        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        if (data != null) {
-            try {
-                for (int i = 0; i < data.length; i++) {
-                    NdefRecord [] recs = ((NdefMessage)data[i]).getRecords();
-                    for (int j = 0; j < recs.length; j++) {
-                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
-                                Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
-                            byte[] payload = recs[j].getPayload();
-                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
-                            int langCodeLen = payload[0] & 0077;
-
-                            s += ("\n\nNdefMessage[" + i + "], NdefRecord[" + j + "]:\n\"" +
-                                    new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1,
-                                            textEncoding) + "\"");
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                Log.e("TagDispatch", e.toString());
-            }
-        }
-
-        Log.i(TAG, "SUCCESS!");
-        //mTextView.setText(s);
     }
-
 
 
     @Override
     public void onPause() {
         super.onPause();
-
-        if (nfcDevice != null)
-            nfcDevice.disableForegroundDispatch(this);
     }
 
 
